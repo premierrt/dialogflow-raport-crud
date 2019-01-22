7fa48b388922693b44f3bda39f613046e445cdb4
rafal_ubuntu_praca <rafal@gmail>
2019-01-22 17:51:35 +0100

    crud

---
+++ src/main/java/dialogflow/api/ActivityHistoryCrudApi.java
@@ -0,0 +1,33 @@
+package dialogflow.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import dialogflow.DialogflowRaportCrudApplication;
+import dialogflow.model.ActivityEntry;
+import dialogflow.model.ActivityRepozytory;
+
+@RestController
+public class ActivityHistoryCrudApi {
+
+	@Autowired
+	ActivityRepozytory activityRepozytory;
+	
+	@PostMapping("/entries")
+	public ResponseEntity<ActivityEntry> saveActivity(@RequestBody ActivityEntry activityEntry ){
+		ActivityEntry newEntry= activityRepozytory.save(activityEntry);
+		return ResponseEntity.ok().body(newEntry);
+		
+	}
+	
+	@GetMapping("/entries")
+	public List<ActivityEntry> getActivityEntries() {
+		return (List<ActivityEntry>) activityRepozytory.findAll();
+	}
+}
---
+++ src/main/java/dialogflow/model/ActivityRepozytory.java
@@ -0,0 +1,9 @@
+package dialogflow.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ActivityRepozytory extends CrudRepository<ActivityEntry, Long>{
+
+	
+	
+}
