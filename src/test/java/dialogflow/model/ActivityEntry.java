bb6bc74f3f496714d95d16713078e99bcf222c3a
rafal_ubuntu_praca <rafal@gmail>
2019-01-22 13:28:49 +0100

    lombok

--- src/test/java/dialogflow/model/ActivityEntry.java
+++ src/test/java/dialogflow/model/ActivityEntry.java
@@ -6,12 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import lombok.Data;
+
 @Entity
+@Data
 public class ActivityEntry {
 
 	@Id
 	@GeneratedValue
-	@Data
 	private Long id;
 	
 	private String name;
