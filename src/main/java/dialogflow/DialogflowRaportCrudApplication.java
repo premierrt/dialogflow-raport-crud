0c21ca90223f056853537a9a50a38a5c20702388
rafal_ubuntu_praca <rafal@gmail>
2019-01-22 12:09:42 +0100

    init

---
+++ src/main/java/dialogflow/DialogflowRaportCrudApplication.java
@@ -0,0 +1,14 @@
+package dialogflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DialogflowRaportCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DialogflowRaportCrudApplication.class, args);
+	}
+
+}
+
