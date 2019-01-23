5b0f662e4e7a9039c926676affa3bacc72643849
rafal_ubuntu_praca <rafal@gmail>
2019-01-23 12:20:30 +0100

    test

--- src/main/java/dialogflow/model/ActivityEntry.java
+++ src/main/java/dialogflow/model/ActivityEntry.java
@@ -6,8 +6,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Data;
 
+@JsonIgnoreProperties
 @Entity
 @Data
 public class ActivityEntry {
